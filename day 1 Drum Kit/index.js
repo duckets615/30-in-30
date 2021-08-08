48e8b52daab46b372258bd71098b60e68f86bfda
ChrisReilly024 <chris.reilly024@gmail.com>
2021-08-08 04:59:09 -0400

    keydown event listener hookup, play function implemented

--- day 1 Drum Kit/index.js
+++ day 1 Drum Kit/index.js
@@ -1,1 +1,5 @@
-// console.log('working')
+window.addEventListener('keydown', (e) => {
+    console.log(e.key)
+    const sound = document.querySelector(`audio[data-key="${e.key}"`);
+    sound.play();
+});
